752a977c926fcfdbaed34bdcd22b27e24dc7ad88
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-16 15:29:51 +0800

    制作CopyUtil类封装BeanUtils

--- src/main/java/top/kaluna/wiki/service/EbookService.java
+++ src/main/java/top/kaluna/wiki/service/EbookService.java
@@ -1,15 +1,14 @@
 package top.kaluna.wiki.service;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import top.kaluna.wiki.domain.Ebook;
 import top.kaluna.wiki.domain.EbookExample;
 import top.kaluna.wiki.mapper.EbookMapper;
 import top.kaluna.wiki.req.EbookReq;
 import top.kaluna.wiki.resp.EbookResp;
+import top.kaluna.wiki.util.CopyUtil;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,13 +26,7 @@ public class EbookService {
         criteria.andNameLike("%"+ebookReq.getName()+"%");
         final List<Ebook> ebooks = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respsList = new ArrayList<>();
-        for (Ebook ebook :
-                ebooks) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respsList.add(ebookResp);
-        }
+        final List<EbookResp> respsList = CopyUtil.copyList(ebooks, EbookResp.class);
         return respsList;
     }
 }
