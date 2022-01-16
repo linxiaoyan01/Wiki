ae49e6e6ecf7c5304ff67b61c2dfcd181fc19019
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-16 13:47:09 +0800

    增加demo表列表查询

--- src/main/java/top/kaluna/wiki/controller/DemoController.java
+++ src/main/java/top/kaluna/wiki/controller/DemoController.java
@@ -1,8 +1,28 @@
 package top.kaluna.wiki.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.kaluna.wiki.domain.Demo;
+import top.kaluna.wiki.service.DemoService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * @author Yuery
  * @date 2022/1/15/0015 - 23:22
  */
+@RestController
+@RequestMapping("/demo")
 public class DemoController {
+
+
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list(){
+        return demoService.list();
+    }
 }
--- src/main/java/top/kaluna/wiki/service/DemoService.java
+++ src/main/java/top/kaluna/wiki/service/DemoService.java
@@ -1,8 +1,22 @@
 package top.kaluna.wiki.service;
 
+import org.springframework.stereotype.Service;
+import top.kaluna.wiki.domain.Demo;
+import top.kaluna.wiki.mapper.DemoMapper;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * @author Yuery
  * @date 2022/1/15/0015 - 23:35
  */
+@Service
 public class DemoService {
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
 }
