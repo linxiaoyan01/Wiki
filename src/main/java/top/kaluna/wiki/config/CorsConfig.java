522175bc93d56fa0f023903f50ae7b534c684516
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-16 19:55:35 +0800

    使用axios调用后端接口/ebook/list 创建CorsConfig解决跨域问题

--- src/main/java/top/kaluna/wiki/config/CorsConfig.java
+++ src/main/java/top/kaluna/wiki/config/CorsConfig.java
@@ -1,8 +1,25 @@
 package top.kaluna.wiki.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * @author Yuery
  * @date 2022/1/15/0015 - 23:23
  */
-public class CorsConfig {
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                //一小时内不需要再预检，发OPTION请求
+                .maxAge(3600);
+    }
 }
