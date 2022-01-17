d19d256582d818177086b29fa6d05c0cd8398352
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-17 11:46:23 +0800

    增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器LogFilter和拦截器LogInterceptor和SpringMvcConfig类

--- src/main/java/top/kaluna/wiki/config/SpringMvcConfig.java
+++ src/main/java/top/kaluna/wiki/config/SpringMvcConfig.java
@@ -1,26 +1,26 @@
-package top.kaluna.wiki.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import top.kaluna.wiki.interceptor.LogInterceptor;
-
-import javax.annotation.Resource;
-
-/**
- * @author Yuery
- * @date 2022/1/15/0015 - 23:24
- */
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-
-}
+//package top.kaluna.wiki.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import top.kaluna.wiki.interceptor.LogInterceptor;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * @author Yuery
+// * @date 2022/1/15/0015 - 23:24
+// */
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry){
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//
+//}
