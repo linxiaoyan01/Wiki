a8e0182b43587f8b92f750c1b38a3751835ccd7e
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-17 09:38:03 +0800

    增加过滤器，打印接口耗时

--- src/main/java/top/kaluna/wiki/filter/LogFilter.java
+++ src/main/java/top/kaluna/wiki/filter/LogFilter.java
@@ -1,8 +1,43 @@
 package top.kaluna.wiki.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
 /**
  * @author Yuery
  * @date 2022/1/15/0015 - 23:27
  */
-public class LogFilter {
+@Component
+public class LogFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        //打印请求信息
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("------ LogFilter开始 ------");
+        LOG.info("请求地址：{} {}",request.getRequestURL().toString(),request.getMethod());
+        LOG.info("远程地址:{}",request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest,servletResponse);
+        LOG.info("------LogFilter结束耗时:{}ms------",System.currentTimeMillis() - startTime);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
