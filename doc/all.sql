e33ca7e7024d9f61f588853c05f4bd9fa214d9d7
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-19 22:46:52 +0800

    文档内容表的设计与代码生成

--- doc/all.sql
+++ doc/all.sql
@@ -82,3 +82,10 @@ insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) val
 
 insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (5, 1, 0, '文档3',2, 0,0);
 insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, '文档3.1',1, 0,0);
+
+drop table if exists `content`;
+create table `content`(
+    `id` bigint not null comment '文档id',
+    `content` mediumtext not null comment '内容',
+    primary key (`id`)
+)engine=innodb default charset = utf8mb4 comment = '文档内容';
