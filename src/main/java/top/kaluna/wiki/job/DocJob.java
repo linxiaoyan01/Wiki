7ba77bd7b50827c46983e74e0ccce1f7b2fe8a75
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-22 13:40:15 +0800

    增加定时器，定时执行电子书信息更新SQL

--- src/main/java/top/kaluna/wiki/job/DocJob.java
+++ src/main/java/top/kaluna/wiki/job/DocJob.java
@@ -1,8 +1,37 @@
 package top.kaluna.wiki.job;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import top.kaluna.wiki.service.DocService;
+import top.kaluna.wiki.util.SnowFlake;
+
+import javax.annotation.Resource;
+
 /**
  * @author Yuery
  * @date 2022/1/15/0015 - 23:28
  */
+@Component
 public class DocJob {
+    private final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+
+    @Resource
+    private DocService docService;
+    @Resource
+    private SnowFlake snowFlake;
+
+    /**
+     * 每30秒更新电子书信息
+     *  从5秒开始,每30秒执行一次
+     */
+    @Scheduled(cron = "5/30 * * * * ?")
+    public void cron(){
+        LOG.info("更新电子书下的文档数据开始");
+        long start = System.currentTimeMillis();
+        docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒",System.currentTimeMillis() - start);
+    }
+
 }
--- src/main/java/top/kaluna/wiki/service/DocService.java
+++ src/main/java/top/kaluna/wiki/service/DocService.java
@@ -134,6 +134,10 @@ public class DocService {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
     }
+
+    public void updateEbookInfo() {
+        docMapperCust.updateEbookInfo();
+    }
 }
 
 
