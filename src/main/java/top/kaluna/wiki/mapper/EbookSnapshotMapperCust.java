c1f7ec770fc990b30484d4ca9ce2aa5f2b19d593
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-22 23:32:52 +0800

    增加了定时任务，定时收集数据，要在xml里一次执行多个SQL的话，需要为数据库连接增加参数，&allowMultiQueries=true

---
+++ src/main/java/top/kaluna/wiki/mapper/EbookSnapshotMapperCust.java
@@ -0,0 +1,9 @@
+package top.kaluna.wiki.mapper;
+
+/**
+ * @author Yuery
+ * @date 2022/1/22/0022 - 23:21
+ */
+public interface EbookSnapshotMapperCust {
+    void genSnapshot();
+}
--- src/main/java/top/kaluna/wiki/service/EbookSnapshotService.java
+++ src/main/java/top/kaluna/wiki/service/EbookSnapshotService.java
@@ -1,8 +1,21 @@
 package top.kaluna.wiki.service;
 
+import org.springframework.stereotype.Service;
+import top.kaluna.wiki.mapper.EbookSnapshotMapperCust;
+
+import javax.annotation.Resource;
+
 /**
  * @author Yuery
  * @date 2022/1/15/0015 - 23:36
  */
+@Service
 public class EbookSnapshotService {
+
+    @Resource
+    private EbookSnapshotMapperCust ebookSnapshotMapperCust;
+
+    public void genSnapshot() {
+        ebookSnapshotMapperCust.genSnapshot();
+    }
 }
