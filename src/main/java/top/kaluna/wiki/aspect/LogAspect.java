2894b7b140a5f223a0fb6ee06226b7a75a5416c6
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-22 15:06:31 +0800

    增加日志流水号，方便生产运维

--- src/main/java/top/kaluna/wiki/aspect/LogAspect.java
+++ src/main/java/top/kaluna/wiki/aspect/LogAspect.java
@@ -10,12 +10,15 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import top.kaluna.wiki.util.RequestContext;
+import top.kaluna.wiki.util.SnowFlake;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -28,6 +31,8 @@ import javax.servlet.http.HttpServletRequest;
 @Aspect
 public class LogAspect {
     private static final Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+    @Resource
+    private SnowFlake snowFlake;
     /**
      * 定义一个切点
      */
@@ -37,6 +42,10 @@ public class LogAspect {
     }
     @Before("controllerPointcut()")
     public void doBefore(JoinPoint joinPoint) throws Throwable{
+
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+
         //开始打印请求日志
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
