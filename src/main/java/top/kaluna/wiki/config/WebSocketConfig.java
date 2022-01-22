c384ea0a162a74b22acff7509438a504d1b54faf
lxy1866 <xiaoyanlin1866@gmail.com>
2022-01-22 16:42:33 +0800

    集成websocket

--- src/main/java/top/kaluna/wiki/config/WebSocketConfig.java
+++ src/main/java/top/kaluna/wiki/config/WebSocketConfig.java
@@ -1,8 +1,19 @@
 package top.kaluna.wiki.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
 /**
  * @author Yuery
  * @date 2022/1/15/0015 - 23:24
  */
+@Configuration
 public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
 }
